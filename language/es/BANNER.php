4a9865a9a67c0a28c1248c33802f06f6ee2ceebc
AleWiccan <32316056+AleWiccan@users.noreply.github.com>
2021-07-07 19:11:04 -0400

    Revert "Add DE"
    This reverts commit 39208dee176c2ba1b11f677c7313c408288521fc.

--- language/es/BANNER.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Traducido al español por AleWiccan;
-// All rights reserved from 2021;
-// Company 1367.
- 
-$LNG['ub_points']		                             = 'Puntos';
-$LNG['ub_fights']		                             = 'Batallas';
-$LNG['ub_quote']		                             = 'Cita';
-$LNG['ub_rank']			          = 'Posición';
